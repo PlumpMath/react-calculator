9b11f35996289ba2cada90eef48baf277b34a880
Patrick Gallagher <paddy.gallagher@gmail.com>
2016-12-05 18:05:44 +0000

    Creating a tree of vanilla React components with a <div> and two children, a <p> and <img> Including work-around as React elements contain a JavaScript symbol and ClojureScript doesn't currently know how to display that.

--- src/cljs/calculator/core.cljs
+++ src/cljs/calculator/core.cljs
@@ -1,6 +1,17 @@
-(ns calculator.core)
+(ns calculator.core
+  (:require cljsjs.react
+            cljsjs.react.dom))
+
+(extend-type js/Symbol
+  IPrintWithWriter
+  (-pr-writer [obj writer _]
+    (write-all writer (str "#object[" (.toString obj) "]"))))
 
 (enable-console-print!)
 
 (set! (.-innerHTML (js/document.getElementById "app"))
-      "<h1>Hello Chestnut!</h1>")
+      "<h1>Hello React! What a great way to develop :)</h1>")
+
+(js/React.createElement "div" #js {}
+                        (js/React.createElement "p" #js {} "Hello from React")
+                        (js/React.createElement "img" #js {:src "/parrot.gif"}))
