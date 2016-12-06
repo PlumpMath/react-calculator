bddbf392c736c29afd888594ed5141311ac163ff
Patrick Gallagher <paddy.gallagher@gmail.com>
2016-12-06 17:30:44 +0000

    Creating digit event-handler Creating operator event-handler Adding a history vector Adding a compute function Showing computed function and reseting history to empty

--- src/cljs/calculator/core.cljs
+++ src/cljs/calculator/core.cljs
@@ -1,6 +1,7 @@
 (ns calculator.core
   (:require cljsjs.react
-            cljsjs.react.dom))
+            cljsjs.react.dom
+            [clojure.string :as str]))
 
 (extend-type js/Symbol
   IPrintWithWriter
@@ -9,7 +10,7 @@
 
 (enable-console-print!)
 
-(defonce app-state (atom {:display 0}))
+(def app-state (atom {:display 0 :history []}))
 
 (defn element [type props & children]
   "Create a Html element with associated attributes and children elements"
@@ -23,51 +24,82 @@
                   (this-as t
                     (render (js->clj (.-props t) :keywordize-keys true))))}))
 
+
+
 (def History
   (component "History"
              :render (fn [props]
-                       (element "div" {:className "history"} "5 + 20 +"))))
+                       (element "div" {:className "history"} (str/join " " (props :history))))))
 
 (def Display
   (component "Display"
              :render (fn [props]
                        (element "div" {:className "display"} (props :value)))))
 
+
+(defn digit-pressed [digit]
+  (swap! app-state update :display #(long (str % digit))))
+
 (def Button
   (component "Button"
              :render (fn [props]
-                       (element "button" {:className "button"} "-"))))
+                       (let [label (props :label)
+                             handler (props :onPress)]
+                         (element "button" {:className "button"
+                                            :onClick #(handler label)} label)))))
+
+(defn operator-pressed [op]
+  (swap! app-state (fn [state]
+                     (-> state
+                         (update :history #(conj % (:display state) op))
+                         (assoc :display 0)))))
+
+(defn compute [result & [op num & xs]]
+  (case op
+    "+" (recur (+ result num) xs)
+    "-" (recur (- result num) xs)
+    "/" (recur (/ result num) xs)
+    "*" (recur (* result num) xs)
+    result))
+
+(defn equals-pressed [_]
+  (swap! app-state (fn [state]
+                     (let [history (conj (:history state) (:display state))
+                           result (apply compute history)]
+                       (-> state
+                           (assoc :display result)
+                           (assoc :history []))))))
 
 (def Keypad
   (component "Keypad"
              :render (fn [props]
                        (element "div" {}
                                 (element "div" {}
-                                         (element Button {})
-                                         (element Button {})
-                                         (element Button {})
-                                         (element Button {}))
+                                         (element Button {:label "7" :onPress digit-pressed})
+                                         (element Button {:label "8" :onPress digit-pressed})
+                                         (element Button {:label "9" :onPress digit-pressed})
+                                         (element Button {:label "/" :onPress operator-pressed}))
                                 (element "div" {}
-                                         (element Button {})
-                                         (element Button {})
-                                         (element Button {})
-                                         (element Button {}))
+                                         (element Button {:label "4" :onPress digit-pressed})
+                                         (element Button {:label "5" :onPress digit-pressed})
+                                         (element Button {:label "6" :onPress digit-pressed})
+                                         (element Button {:label "*" :onPress operator-pressed}))
                                 (element "div" {}
-                                         (element Button {})
-                                         (element Button {})
-                                         (element Button {})
-                                         (element Button {}))
+                                         (element Button {:label "1" :onPress digit-pressed})
+                                         (element Button {:label "2" :onPress digit-pressed})
+                                         (element Button {:label "3" :onPress digit-pressed})
+                                         (element Button {:label "-":onPress operator-pressed}))
                                 (element "div" {}
-                                         (element Button {})
-                                         (element Button {})
-                                         (element Button {})
-                                         (element Button {}))))))
+                                         (element Button {:label "0" :onPress digit-pressed})
+                                         (element Button {:label "."})
+                                         (element Button {:label "="})
+                                         (element Button {:label "+" :onPress operator-pressed}))))))
 
 (def Calculator
   (component "Calculator"
              :render (fn [props]
                        (element "div" {}
-                                (element History {})
+                                (element History {:history (props :history)})
                                 (element Display {:value (props :display)})
                                 (element Keypad {})))))
 
